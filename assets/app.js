6ac1e2899cb01d5f27ebc71a0fb9ae76b0dbd8c2
Mehwish <mehwishgul715@gmail.com>
2023-01-28 10:11:04 +0500

    commit

--- assets/app.js
+++ assets/app.js
@@ -1,69 +1,130 @@
-const imageGrid = document.querySelector(".image-grid");
-const links = imageGrid.querySelectorAll("a");
-const imgs = imageGrid.querySelectorAll("img");
-const lightboxModal = document.getElementById("lightbox-modal");
-const bsModal = new bootstrap.Modal(lightboxModal);
-const modalBody = document.querySelector(".modal-body .container-fluid");
 
-for (const link of links) {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const currentImg = link.querySelector("img");
-    const lightboxCarousel = document.getElementById("lightboxCarousel");
-    if (lightboxCarousel) {
-      const parentCol = link.parentElement.parentElement;
-      const index = [...parentCol.parentElement.children].indexOf(parentCol);
-      const bsCarousel = new bootstrap.Carousel(lightboxCarousel);
-      bsCarousel.to(index);
-    } else {
-      createCarousel(currentImg);
+$(document).ready(function () {
+    // init Isotope
+    var $grid = $('.grid').isotope({
+        itemSelector: '.grid-item',
+        percentPosition: true,
+
+        masonry: {
+            columnWidth: '.grid-sizer'
+        },
+
+    });
+    // layout Isotope after each image loads
+    $grid.imagesLoaded().progress(function () {
+        $grid.isotope('layout');
+    });
+    // filter functions
+    var filterFns = {
+        // show if number is greater than 50
+        numberGreaterThan50: function () {
+            var number = $(this).find('.number').text();
+            return parseInt(number, 10) > 50;
+        },
+
+    };
+
+    // bind filter button click
+    $('#filters').on('click', 'button', function () {
+        var filterValue = $(this).attr('data-filter');
+        // use filterFn if matches value
+        filterValue = filterFns[filterValue] || filterValue;
+        $grid.isotope({ filter: filterValue });
+    });
+
+
+    var x, i, j, l, ll, selElmnt, a, b, c;
+    /* Look for any elements with the class "custom-select": */
+    x = document.getElementsByClassName("custom-select");
+    l = x.length;
+    for (i = 0; i < l; i++) {
+        selElmnt = x[i].getElementsByTagName("select")[0];
+        ll = selElmnt.length;
+        /* For each element, create a new DIV that will act as the selected item: */
+        a = document.createElement("DIV");
+        a.setAttribute("class", "select-selected");
+        a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+        x[i].appendChild(a);
+        /* For each element, create a new DIV that will contain the option list: */
+        b = document.createElement("DIV");
+        b.setAttribute("class", "select-items select-hide");
+        for (j = 1; j < ll; j++) {
+            /* For each option in the original select element,
+            create a new DIV that will act as an option item: */
+            c = document.createElement("DIV");
+            c.innerHTML = selElmnt.options[j].innerHTML;
+            c.addEventListener("click", function (e) {
+                /* When an item is clicked, update the original select box,
+                and the selected item: */
+                var y, i, k, s, h, sl, yl;
+                s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+                sl = s.length;
+                h = this.parentNode.previousSibling;
+                for (i = 0; i < sl; i++) {
+                    if (s.options[i].innerHTML == this.innerHTML) {
+                        s.selectedIndex = i;
+                        h.innerHTML = this.innerHTML;
+                        y = this.parentNode.getElementsByClassName("same-as-selected");
+                        yl = y.length;
+                        for (k = 0; k < yl; k++) {
+                            y[k].removeAttribute("class");
+                        }
+                        this.setAttribute("class", "same-as-selected");
+                        break;
+                    }
+                }
+                h.click();
+            });
+            b.appendChild(c);
+        }
+        x[i].appendChild(b);
+        a.addEventListener("click", function (e) {
+            /* When the select box is clicked, close any other select boxes,
+            and open/close the current select box: */
+            e.stopPropagation();
+            closeAllSelect(this);
+            this.nextSibling.classList.toggle("select-hide");
+            this.classList.toggle("select-arrow-active");
+        });
     }
-    bsModal.show();
+
+    function closeAllSelect(elmnt) {
+        /* A function that will close all select boxes in the document,
+        except the current select box: */
+        var x, y, i, xl, yl, arrNo = [];
+        x = document.getElementsByClassName("select-items");
+        y = document.getElementsByClassName("select-selected");
+        xl = x.length;
+        yl = y.length;
+        for (i = 0; i < yl; i++) {
+            if (elmnt == y[i]) {
+                arrNo.push(i)
+            } else {
+                y[i].classList.remove("select-arrow-active");
+            }
+        }
+        for (i = 0; i < xl; i++) {
+            if (arrNo.indexOf(i)) {
+                x[i].classList.add("select-hide");
+            }
+        }
+    }
+
+    /* If the user clicks anywhere outside the select box,
+    then close all select boxes: */
+    document.addEventListener("click", closeAllSelect);
+});
+
+
+
+
+// ==========================active nav
+$(document).ready(function () {
+ 
+  $('.filter-btns')
+          .click(function (e) {
+      $('.filter-btns')
+          .removeClass('active-category');
+      $(this).addClass('active-category');
   });
-}
-
-function createCarousel(img) {
-  const markup = `
-    <div id="lightboxCarousel" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="false">
-      <div class="carousel-inner">
-        ${createSlides(img)}
-      </div> 
-      <button class="carousel-control-prev" type="button" data-bs-target="#lightboxCarousel" data-bs-slide="prev">
-       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-       <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#lightboxCarousel" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
-    `;
-
-  modalBody.innerHTML = markup;
-}
-
-function createSlides(img) {
-  let markup = "";
-  const currentImgSrc = img.getAttribute("src");
-
-  for (const img of imgs) {
-    const imgSrc = img.getAttribute("src");
-    const imgAlt = img.getAttribute("alt");
-    const imgCaption = img.getAttribute("data-caption");
-
-    markup += `
-    <div class="carousel-item${currentImgSrc === imgSrc ? " active" : ""}">
-      <img src=${imgSrc} alt=${imgAlt}>
-      ${imgCaption ? createCaption(imgCaption) : ""}
-    </div>
-    `;
-  }
-
-  return markup;
-}
-
-function createCaption(caption) {
-  return `<div class="carousel-caption">
-     <p class="m-0">${caption}</p>
-    </div>`;
-}
+});
